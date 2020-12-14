9cd7f35a44dcc16ed77b21583a5e4757a5579df2
FW-Swart <folkert@swartwerk.nl>
2020-12-14 21:57:59 +0100

    main pagees styling

--- partials/header-brand.php
+++ partials/header-brand.php
@@ -17,13 +17,19 @@
             <div class="colum-6 cv-info">
 
                     <button class="btn btn-s-mainf mx-1 p-1">
-                        <a class="btn-s-mainf brand-zw mx-1 p-1"  href="about.php">info<br>CV-<i>plus</i></a>
+                        <a class="btn-s-mainf brand-zw mx-1 p-1"  
+                            href="about.php">info<br>CV-<i>plus</i>
+                        </a>
                     </button>
                     <button class="btn btn-s-mainf mx-1 p-1">
-                        <a class="btn-s-mainf brand-zw mx-1 p-1"  href="mailto:info@cv-plus.nl">e-mail<br>CV-<i>plus</i></a>
+                        <a class="btn-s-mainf brand-zw mx-1 p-1"  
+                            href="mailto:info@cv-plus.nl">e-mail<br>CV-<i>plus</i>
+                        </a>
                     </button>
                     <button class="btn btn-s-mainf mx-1 p-1">
-                        <a class="btn-s-mainf brand-zw mx-1 p-1"  href="#"><sup>Admin</sup></a>
+                        <a class="btn-s-mainf brand-zw mx-1 p-1"  
+                            href="#"><sup>Admin</sup>
+                        </a>
                     </button>                
                 
             
