0d0912286d8e484d973a949bb875711745879f10
FW-Swart <folkert@swartwerk.nl>
2020-12-31 10:06:31 +0100

    register form and invite login views toegevoegd

--- routes.php
+++ routes.php
@@ -5,14 +5,16 @@ $router->get('home', 'app/Controllers/HomeController.php');
 
 
 $router->get('login', 'app/Controllers/LoginController.php');
-$router->get('logout', 'App/Controllers/LoginController.php@logout');
-$router->post('login/auth', 'App/Controllers/LoginController.php@login');
+$router->get('logout', 'app/Controllers/LoginController.php@logout');
+$router->post('login/auth', 'app/Controllers/LoginController.php@login');
 
 
 $router->get('register', 'app/Controllers/RegisterController.php');
 
 
 $router->get('invite', 'app/Controllers/InviteController.php');
+$router->get('logout', 'app/Controllers/InviteController.php@logout');
+$router->post('invite/auth', 'app/Controllers/InviteController.php@login');
 
 
 $router->get('cv-home-make', 'app/Controllers/CvmakeController.php');
